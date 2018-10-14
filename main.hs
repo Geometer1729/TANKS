a7e82c17e8be721098e389a852d842a513c0a810
Geometer1729 <kuhnsbt@clarkson.edu>
2018-10-13 21:46:21 -0400

    finna fix my changes

--- main.hs
+++ main.hs
@@ -2,8 +2,7 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Data.Word
 import Debug.Trace
-
-data Instruction = Sit | Shoot | Scan Float Float | Aim Float | Move
+import Assembly
 
 type Team = Int
 
@@ -15,8 +14,8 @@ data Tank = Tank{
 	pos :: Point,
 	angle :: Float,
 	team :: Team,
-	memory :: Word64
-}deriving(Show)
+	memory :: Runtime
+}
 
 data Bullet = Bullet {
 	bpos :: Point,
@@ -31,13 +30,7 @@ data World = World {
 
 tau = pi*2
 
-test1 :: Tank
-test1 = Tank (10,10) 0 0 0
-
-test2 :: Tank
-test2 = Tank (490,490) pi 1 0
-
-world = World [test1,test2] [] 1000
+world = World [] [] 1000
 
 render :: World -> IO Picture
 render w = return $ translate (-size w/2) (-size w/2) $ Pictures $ (map drawTank (tanks w)) ++ (map drawBullet (bulets w))
@@ -61,12 +54,12 @@ handle :: Event -> World -> IO World
 handle _ w = return w 	
 
 step :: Float -> World -> IO World
-step _ w = trace (show (ts,nts,(bulets w),bs)) return w{tanks = sts, bulets = filter bulletInMap $ map stepBullet $ nbs} 
+step _ w = return w{tanks = sts, bulets = filter bulletInMap $ map stepBullet $ nbs} 
 	where
 		sts = filter (notShot nbs) nts
 		nbs = (bulets w) ++ (map stepBulleter bs)
 		ts = tanks w :: [Tank]
-		is = zipWith (\(i,m) t -> (t{memory=m},i)) (map (ai.memory) ts) ts :: [(Tank,Instruction)]
+		is = zipWith (\(m,i) t -> (t{memory=m},i)) (map (run .memory) ts) ts :: [(Tank,HardInst)]
 		(nts,bs) = handleTanks is	
 
 bulletInMap :: Bullet -> Bool
@@ -76,11 +69,11 @@ notShot :: [Bullet] -> Tank -> Bool
 notShot bs (Tank (xt,yt) _ _ _) = not $ or [ abs (xb - xt) < 10 && abs (yb - yt) < 10 | (Bullet (xb,yb) _)  <- bs] 
 
 
-handleTanks :: [(Tank,Instruction)]-> ([Tank],[Bullet])
+handleTanks :: [(Tank,HardInst)]-> ([Tank],[Bullet])
 handleTanks [] = ([],[])
 handleTanks ((t,i):its) = tankHelper i (t,its,[],[])
 
-tankHelper :: Instruction -> (Tank,[(Tank,Instruction)],[Tank],[Bullet]) -> ([Tank],[Bullet])
+tankHelper :: HardInst -> (Tank,[(Tank,HardInst)],[Tank],[Bullet]) -> ([Tank],[Bullet])
 tankHelper i (t,[],ts,bs) = (nt:ts,nbs)
 	where
 		(nt,_,nbs) = tankDo i (t,[],ts,bs)
@@ -90,16 +83,27 @@ tankHelper i (t,its,ts,bs) = tankHelper nni (nnt,tail its,nt:ts,nbs)
 		(nnt,nni) = head its
 		
 
-tankDo :: Instruction -> (Tank,[(Tank,Instruction)],[Tank],[Bullet]) -> (Tank,[(Tank,Instruction)],[Bullet])
+tankDo :: HardInst -> (Tank,[(Tank,HardInst)],[Tank],[Bullet]) -> (Tank,[(Tank,HardInst)],[Bullet])
 tankDo Sit (t,its,_,bs) = (t,its,bs)
-tankDo (Aim a) (t,its,_,bs) = (t{angle=a},its,bs)
-tankDo (Scan a b) (t,its,ts,bs) = (t{memory = readScan (scanWorld t a b ts) (memory t) },its,bs)
-tankDo Move (t,its,_,bs) = (t{pos = (x,y)} ,its,bs)
+tankDo (HAim a) (t,its,_,bs) = (t{angle=a},its,bs)
+tankDo (HScan a b) (t,its,ts,bs) = (t{memory = setRegister (setRegister (memory t) (V ra) "a") (V rb) "b"},its,bs)
+	where
+		(ra,rb) = scanWorld t a b ts
+		
+tankDo HMove (t,its,_,bs) = (t{pos = (x,y)} ,its,bs)
 	where
 		(tx,ty) = pos t
 		ta = angle t
-		(x,y) = (tx + (cos ta),ty + (sin ta)) 
+		(x,y) = (tx + (cos ta),ty + (sin ta))
 tankDo Shoot (t,its,ts,bs) = (t,its, Bullet (pos t) (angle t):bs)
+tankDo HGPS (t,its,ts,bs) = (t{memory = nm},its,bs)
+	where
+		(tx,ty) =  pos t
+		nm = setRegister (setRegister (memory t) (V $ round tx) "a") (V $ round ty) "b"
+tankDo HGyro (t,its,ts,bs) = (t{memory = nm},its,bs) 
+	where 
+		ta = angle t
+		nm = setRegister (memory t) (V $ round ta) "a"
 
 shoots :: Tank -> Tank -> Bool
 shoots t1 t2 = abs ((angle t1) - (getAngle t1 t2)) < (pi / 512)
@@ -111,14 +115,10 @@ getAngle t1 t2 = ca
 		(x2,y2) = pos t2
 		ca = (if y2 > y1 then 0 else pi)  + atan ((y2-y1)/(x2-x1)) 
 
-scanWorld :: Tank -> Float -> Float -> [Tank] -> Bool
-scanWorld t1 a b ts = or [ (a < (getAngle t1 t2)) && ((getAngle t1 t2) < b) | t2 <- ts  ]
-
-ai :: Word64 -> (Instruction,Word64)
-ai n = if n < 4096 then ( if mod n 2 == 0 then (Aim ( (fromIntegral) n * (tau/4096)) ,n+1) else (Shoot,n+1) ) else (Aim 0,0)
-
-readScan :: Bool -> Word64 -> Word64
-readScan s m =  2 * (div m 2) + if s then 1 else m
+scanWorld :: Tank -> Float -> Float -> [Tank] -> (Int,Int)
+scanWorld t1 a b ts = (length $ filter (\t -> team t == team t1) sTs, length $ filter (\t -> team t /= team t1) sTs)
+	where
+		sTs =  [ t2 | t2 <- ts , (a < (getAngle t1 t2)) && ((getAngle t1 t2) < b) ]
 
 main = playIO (InWindow "TANKS!" (1000,1000) (40,40)) white 30 world render handle step 
 
